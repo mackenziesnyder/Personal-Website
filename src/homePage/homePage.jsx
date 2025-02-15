bac11c888eab3a195acd4c4e3851d9008ead77f2
Mackenzie Snyder <masnyder2@hotmail.com>
2025-02-15 17:03:05 -0500

    inital commit of new website changes

--- src/homePage/homePage.jsx
+++ src/homePage/homePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './homePage.css';
-import headShot from './content/head-shot.png';
+import headShot from './content/linkedin-pic.png';
 import linkedInIcon from './content/Linkedin-icon.jpg';
 import githubIcon from './content/github.jpg';
 import mailIcon from './content/mail-icon.jpg';
@@ -10,32 +10,25 @@ function homePage() {
     return (
       <div className='homePage'>
         <div className='homePageContainer'>
-          <div className='title'>Mackenzie Snyder</div>
-          <p className='description'>Biomedical Engineering Student with a Passion in Robotics, Automation, and Technologically Advancing Healthcare</p>
+          <div className='title'>Hi I'm Mackenzie</div>
+          <div className='sub-title'>I'm a 4th Year Engineering Student</div>
+          <div className='description'>I am experienced in software development in industry and research settings, focusing on medical technology applications</div>
           <div className='link-cont'>
             <div className='item-cont'>
-              <img className="icon" src={linkedInIcon} alt="linkedIn Icon"></img>
-              <div className="link">
-                  <a  href="https://www.linkedin.com/in/mackenzie-snyder-a92178220/" target="_blank" rel="noopener noreferrer">
-                      Linkedin
-                  </a>
-              </div>
+            <a href="https://www.linkedin.com/in/mackenzie-snyder-a92178220/" target="_blank" rel="noopener noreferrer">
+              <img className="icon" src={linkedInIcon} alt="linkedIn Icon" href="https://www.linkedin.com/in/mackenzie-snyder-a92178220/">
+              </img>
+            </a>
             </div>
             <div className="item-cont">
-              <img className="icon" src={githubIcon} alt="Github Icon"></img>
-              <div className="link">
-                  <a  href="https://github.com/mackenziesnyder" target="_blank" rel="noopener noreferrer">
-                      GitHub
-                  </a>
-              </div>
+              <a href="https://github.com/mackenziesnyder" target="_blank" rel="noopener noreferrer">
+                <img className="icon" src={githubIcon} alt="Github Icon"></img>
+              </a>
             </div>
             <div className="item-cont">
-              <img className="mail-icon" src={mailIcon} alt="Mail Icon"></img>
-              <div className="link">
-                  <a  href="mailto:mackenziesnyder222@gmail.com" target="_blank" rel="noopener noreferrer">
-                      Mail
-                  </a>
-              </div>
+              <a href="mailto:mackenziesnyder222@gmail.com" target="_blank" rel="noopener noreferrer">
+                <img className="mail-icon" src={mailIcon} alt="Mail Icon"></img>
+              </a>
             </div>
           </div>
         </div>
