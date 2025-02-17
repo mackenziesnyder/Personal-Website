8b9931a3745cce81d522aa46073782ef3a4081b7
Mackenzie Snyder <masnyder2@hotmail.com>
2025-02-17 16:21:43 -0500

    Finished-home-page

--- src/homePage/homePage.jsx
+++ src/homePage/homePage.jsx
@@ -1,36 +1,42 @@
 import React from 'react';
 import './homePage.css';
 import headShot from './content/linkedin-pic.png';
-import linkedInIcon from './content/Linkedin-icon.jpg';
-import githubIcon from './content/github.jpg';
-import mailIcon from './content/mail-icon.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faGear, faCodeBranch, faBrain } from '@fortawesome/free-solid-svg-icons';
 
 function homePage() {
   
     return (
       <div className='homePage'>
         <div className='homePageContainer'>
-          <div className='title'>Hi I'm Mackenzie</div>
+          <div className='title'>Hi I'm <span className='name-highlight'>Mackenzie</span>
+              <FontAwesomeIcon icon={faGear} className='symbol-gear'/>
+              <FontAwesomeIcon icon={faCodeBranch} className='symbol-code' />
+              <FontAwesomeIcon icon={faBrain} className='symbol-brain' />
+          </div>
           <div className='sub-title'>I'm a 4th Year Engineering Student</div>
           <div className='description'>I am experienced in software development in industry and research settings, focusing on medical technology applications</div>
           <div className='link-cont'>
             <div className='item-cont'>
-            <a href="https://www.linkedin.com/in/mackenzie-snyder-a92178220/" target="_blank" rel="noopener noreferrer">
-              <img className="icon" src={linkedInIcon} alt="linkedIn Icon" href="https://www.linkedin.com/in/mackenzie-snyder-a92178220/">
-              </img>
-            </a>
+              <a href="https://www.linkedin.com/in/mackenzie-snyder-a92178220/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} className="icon-style" />
+              </a>
             </div>
             <div className="item-cont">
               <a href="https://github.com/mackenziesnyder" target="_blank" rel="noopener noreferrer">
-                <img className="icon" src={githubIcon} alt="Github Icon"></img>
+                <FontAwesomeIcon icon={faGithub} className="icon-style" />
               </a>
             </div>
             <div className="item-cont">
               <a href="mailto:mackenziesnyder222@gmail.com" target="_blank" rel="noopener noreferrer">
-                <img className="mail-icon" src={mailIcon} alt="Mail Icon"></img>
+                <FontAwesomeIcon icon={faEnvelope} className="icon-style" />
               </a>
             </div>
           </div>
+          <div className='button-cont'>
+              <button className='download-cv-button'>Download CV</button>
+            </div>
         </div>
         <div className='image-container'>
             <img src={headShot} alt='head-shot' className='head-shot' />
--- src/navBar/navBar.jsx
+++ src/navBar/navBar.jsx
@@ -6,12 +6,16 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
+      <div className='logo-cont'>
+        <button className='logo-button'>Mackenzie</button>
+      </div>
       <ul className="nav-list">
         <li className="nav-item">
           <Link to="/Personal-Website/" className='nav-button' >Home</Link>
-          <Link to="/about" className='nav-button' >About</Link>
+          {/* <Link to="/about" className='nav-button' >About</Link> */}
           <Link to="/experience" className='nav-button' >Experience</Link>
           <Link to="/resume" className='nav-button' >Resume</Link>
+          <Link to="/resume" className='contact-button' >Contact</Link>
         </li>
       </ul>
     </nav>
